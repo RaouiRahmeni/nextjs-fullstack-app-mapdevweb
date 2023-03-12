2de26284ee6589246d28ae9f7d411218de488541
azizba015 <azizba015@gmail.com>
2023-03-12 22:12:07 +0100

    progress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,10 @@
-import type { AppProps } from 'next/app';
-import Layout from '../components/layout/Layout';
-import '../styles/globals.css';
+import { FC } from 'react';
+import 'styles/globals.css';
+import { CustomAppProps } from 'types';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <div>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </div>
-  );
-}
+const MyApp: FC<CustomAppProps> = ({ Component, pageProps }) => {
+  const getLayout = Component.Layout ?? ((page: any) => page);
+  return <div>{getLayout(<Component {...pageProps} />)}</div>;
+};
 
 export default MyApp;
